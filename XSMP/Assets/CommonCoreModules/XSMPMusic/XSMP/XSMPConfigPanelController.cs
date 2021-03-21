855bda732fc902cf1a3db09704f56b7455c19f7e
XCVGVCX <xcvgvcx@gmail.com>
2021-03-21 12:55:58 -0700

    improved the XSMP configuration experience

--- XSMP/Assets/CommonCoreModules/XSMPMusic/XSMP/XSMPConfigPanelController.cs
+++ XSMP/Assets/CommonCoreModules/XSMPMusic/XSMP/XSMPConfigPanelController.cs
@@ -8,12 +8,21 @@ namespace CommonCore.XSMP
 
     public class XSMPConfigPanelController : ConfigSubpanelController
     {
+        private const float UpdateInterval = 1.5f;
+
         [SerializeField]
-        private Toggle XSMPEnableToggle;
+        private Toggle XSMPEnableToggle = null;
+        [SerializeField]
+        private Toggle XSMPIngameToggle = null;
+        [SerializeField]
+        private Text XSMPStatusText = null;
+
+        private float Elapsed = 0;
 
         public override void PaintValues()
         {
-            XSMPEnableToggle.isOn = ConfigState.Instance.CustomConfigFlags.Contains("XSMPEnabled");
+            XSMPEnableToggle.SetIsOnWithoutNotify(ConfigState.Instance.CustomConfigFlags.Contains("XSMPEnabled"));
+            XSMPIngameToggle.SetIsOnWithoutNotify(ConfigState.Instance.CustomConfigFlags.Contains("XSMPUseIngame"));
         }
 
         public override void UpdateValues()
@@ -23,6 +32,38 @@ namespace CommonCore.XSMP
             else if (!XSMPEnableToggle.isOn && ConfigState.Instance.CustomConfigFlags.Contains("XSMPEnabled"))
                 ConfigState.Instance.CustomConfigFlags.Remove("XSMPEnabled");
 
+            if (XSMPIngameToggle.isOn && !ConfigState.Instance.CustomConfigFlags.Contains("XSMPUseIngame"))
+                ConfigState.Instance.CustomConfigFlags.Add("XSMPUseIngame");
+            else if (!XSMPIngameToggle.isOn && ConfigState.Instance.CustomConfigFlags.Contains("XSMPUseIngame"))
+                ConfigState.Instance.CustomConfigFlags.Remove("XSMPUseIngame");
+
+        }
+
+        public void HandleEnableToggleChanged()
+        {
+            SignalPendingChanges(PendingChangesFlags.RequiresRestart);
+        }
+
+        private void Update()
+        {
+            Elapsed += Time.unscaledDeltaTime;
+            if (Elapsed >= UpdateInterval)
+            {
+                UpdateStatus();
+                Elapsed = 0;
+            }
+        }
+
+        private void UpdateStatus()
+        {
+            if(XSMPModule.Instance == null || !XSMPModule.Instance.Enabled)
+            {
+                XSMPStatusText.text = "Module Inactive";
+            }
+            else
+            {
+                XSMPStatusText.text = XSMPModule.Instance.Status.ToString();
+            }
         }
     }
 }
